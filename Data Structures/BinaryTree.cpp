698d263ef28cc62694287f5c33acb9956eafe3d9
Sanket Mhetre <sanket.mhetre@techprimelab.com>
2021-06-17 20:00:23 +0530

    Day 24

---
+++ Data Structures/BinaryTree.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+// Binary Tree :
+
+/*      A binary tree is a tree data structure in which each parent node can have at most two children.
+    Each node of a binary tree consists of three items:
+        1. data item
+        2. address of left child
+        3. address of right child
+
+    Types of Binary Tree :
+
+        1. Full Binary Tree :
+                A full Binary tree is a special type of binary tree in which every parent node/internal node
+            has either two or no children.
+
+        2. Perfect Binary Tree :
+                A perfect binary tree is a type of binary tree in which every internal node has exactly two child nodes
+            and all the leaf nodes are at the same level.
+
+        3. Complete Binary Tree :
+                A complete binary tree is just like a full binary tree, but with two major differences :
+            1) Every level must be completely filled
+            2) All the leaf elements must lean towards the left.
+            3) The last leaf element might not have a right sibling i.e. a complete binary tree does not have
+             to be a full binary tree.
+
+        4. Degenerate or Pathological Tree :
+                A degenerate or pathological tree is the tree having a single child either left or right.
+
+        5. Skewed Binary Tree :
+                A skewed binary tree is a pathological/degenerate tree in which the tree is either dominated
+            by the left nodes or the right nodes.
+                Thus, there are two types of skewed binary tree:
+                    1) left-skewed binary tree
+                    2) right-skewed binary tree.
+
+        6. Balanced Binary Tree :
+                It is a type of binary tree in which the difference between the height of the left and the right subtree
+            for each node is either 0 or 1.
+
+    Binary Tree Applications
+
+        1. For easy and quick access to data
+        2. In router algorithms
+        3. To implement heap data structure
+        4. Syntax tree
+*/
+
+int main()
+{
+
+    return 0;
+}
