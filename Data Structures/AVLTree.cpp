b7bb2bd8411a6d03b53a4665f0add95c3845e25c
Sanket Mhetre <sanket.mhetre@techprimelab.com>
2021-06-23 22:25:56 +0530

    Day 29

---
+++ Data Structures/AVLTree.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+// AVL Tree :
+/*      AVL Tree is a self-balancing binary search tree in which each node maintains extra information called balance factor
+    whose value is either -1, 0 or +1.
+
+    Balance Factor :
+            Balance factor of a node in an AVL tree is the difference between height of the left subtree and that of right subtree of that node.
+
+            Balance Factor = (Height of left subtree - Height of right subtree)
+                              or
+                             (Height of right subtree - Height of left subtree)
+
+            The self balancing property of AVL tree is maintained by the balance factor. The value of balance factor should always be -1, 0 or +1.
+
+            An example of a balanced AVL tree
+
+    Operations on an AVL tree :
+
+            Various operations that can be performed on an AVL tree are:
+                ->Rotating the subtrees in an AVL Tree
+                ->In rotation operation, the positions of the nodes of a subtree are interchanged.
+
+            There are two types of rotations:
+
+                1) Left Rotate
+                ----------------
+
+
+*/
+
+int main()
+{
+
+    return 0;
+}
